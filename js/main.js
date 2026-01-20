a7f24f1e39e20f76cbde335cb6b099e8142366d7
EnriqueLunaH <freelanceenrique@gmail.com>
2026-01-19 18:28:28 -0600

    se reorganizo la información

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const button = document.querySelector('button');
+  const button = document.querySelector('button.btn');
   button.addEventListener('click', () => {
     alert('Bienvenidos al curso');
   });
